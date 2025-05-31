99e896bb105c355130a454cdc1077fff7f42cd2f
Zain Ul Abideen <zainulabideen1060@gmail.com>
2025-06-01 04:23:28 +0500

    applied linter

--- patients/admin.py
+++ patients/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 
-from patients.models import Patient
+from .models import Patient
 
-admin.site.register(Patient)
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "phone_number",
+        "city",
+        "gender",
+        "organization",
+        "created_at",
+    )
+    list_filter = ("gender", "city", "organization", "created_at")
+    search_fields = ("name", "phone_number", "city")
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        ("Personal Info", {"fields": ("name", "phone_number", "gender", "city")}),
+        ("Organization Info", {"fields": ("organization",)}),
+        ("Timestamps", {"fields": ("created_at",)}),
+    )
+
+    ordering = ("-created_at",)  # newest first
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("organization")
